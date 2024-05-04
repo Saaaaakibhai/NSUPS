b972559ff9b38d9eb7dda6a9411afed28525221a
S. M. Mahedi Hasan <69164727+Saaaaakibhai@users.noreply.github.com>
2024-05-05 00:46:58 +0600

    python118leetcode

--- pythonleetcode/118PascalsTriangle.py
+++ pythonleetcode/118PascalsTriangle.py
@@ -1,10 +1,10 @@
 class Solution:
     def generate(self, numRows: int) -> List[List[int]]:
-        res = [[1]]
-        for i in range(numRows - 1):
-            temp = [0] + res[-1] + [0]
-            row = []
-            for j in range(len(res[-1]) + 1):
-                row.append(temp[j] + temp[j + 1])
-            res.append(row)
-            return res
+        output = [[1]]
+        for r in range (2,numRows+1):
+            tmp = [1]
+            for c in range(1,r-1):
+                tmp.append(output[-1][c]+output[-1][c-1])
+            tmp.append(1)
+            output.append(tmp)
+        return output
