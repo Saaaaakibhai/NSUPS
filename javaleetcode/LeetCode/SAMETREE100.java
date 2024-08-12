ea24e163d2eef2f34dae53feeef09d08addbbdeb
S. M. Mahedi Hasan <69164727+Saaaaakibhai@users.noreply.github.com>
2024-08-12 21:58:07 +0600

    100

--- javaleetcode/LeetCode/SAMETREE100.java
+++ javaleetcode/LeetCode/SAMETREE100.java
@@ -1,7 +1,25 @@
 package LeetCode;
+/**
+         * Definition for a binary tree node.
+         * public class TreeNode {
+         * int val;
+         * TreeNode left;
+         * TreeNode right;
+         * TreeNode() {}
+         * TreeNode(int val) { this.val = val; }
+         * TreeNode(int val, TreeNode left, TreeNode right) {
+         * this.val = val;
+         * this.left = left;
+         * this.right = right;
+         * }
+         * }
+         */
+        class Solution {
+            public boolean isSameTree(TreeNode p, TreeNode q) {
+                if(p==null && q==null) return true;
+                if(p==null || q==null) return false;
+                return p.val==q.val && isSameTree(p.left,q.left) && isSameTree(p.right,q.right);
 
-public class SAMETREE100 {
-    public static void main(String[] args) {
-        System.out.println("Hello");
-    }
-}
+            }
+        }
+
